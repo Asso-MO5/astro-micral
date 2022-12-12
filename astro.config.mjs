b8c18df956e6074b75fadca9e41dd4f7ebcc5481
Kazerlelutin <b@bouteiller.contact>
2022-12-12 21:57:36 +0100

    fix(config): env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
-import { BASE_URL } from './src/utils/constants'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   site: 'https://Asso-MO5.github.io',
-  base: BASE_URL,
+  base: process.env.NODE_ENV === 'development' ? '/' : '/astro-micral',
 })
