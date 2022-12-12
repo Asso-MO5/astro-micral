9ba66edeb42a32e728fa82cf332c06880278f96f
Kazerlelutin <b@bouteiller.contact>
2022-12-12 20:41:57 +0100

    feat(ci): add config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-
-import prefetch from "@astrojs/prefetch";
-
+import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(),svelte()]
-});
+  site: 'https://Asso-MO5.github.io',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/astro-micral',
+})
